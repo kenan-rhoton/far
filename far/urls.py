5983635965f795f23260c592cf9486f13c753647
Kenan Rhoton <kenan.rhoton@gmail.com>
2015-09-25 10:47:26 +0200

    Procfile $PORT

--- far/urls.py
+++ far/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^filtre/', include('filtre.urls', namespace="filtre")),
+	url(r'^/', include('filtre.urls', namespace="filtre")),
     url(r'^admin/', include(admin.site.urls)),
 ]
