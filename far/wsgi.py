d84430d21e54ea77893e62d1f578c42009aa9d15
Kenan Rhoton <kenan.rhoton@gmail.com>
2015-11-04 00:16:13 +0100

    According to Heroku...

--- far/wsgi.py
+++ far/wsgi.py
@@ -17,3 +17,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "far.settings")
 from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
