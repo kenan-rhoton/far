439502d4e25aba9809d151560a7202c008ff4c72
Kenan Rhoton <kenan.rhoton@gmail.com>
2016-04-06 19:22:38 +0200

    restructure

--- filtre/views.py
+++ filtre/views.py
@@ -10,49 +10,65 @@ import re
 from .models import Noticia, Font
 
 def index(request):
+    
     noticies = Noticia.objects.order_by('-data')
     fonts = Font.objects.all()
     context = {'noticies': noticies, 'fonts':fonts,}
     return render(request, 'filtre/index.html', context)
 
+
+
 def detall_noticia(request, noticia_id):
+
     noticia = get_object_or_404(Noticia, pk=noticia_id)
     return render(request, 'filtre/detall_noticia.html', {'noticia':noticia, 'fonts': Font.objects.all()})
 
 
+
 def detall_font(request, font_id):
-  font = get_object_or_404(Font, pk=font_id)
-  noticies = font.noticia_set.all()
-  frases = ""
-  for cat in font.cataleg_set.all():
-    frases += cat.frases + ";"
-  return render(request, 'filtre/detall_font.html', {'font':font, 'noticies': noticies, 'fonts': Font.objects.all(), 'frases': frases })
+    
+    font = get_object_or_404(Font, pk=font_id)
+    noticies = font.noticia_set.all()
+    frases = ""
+
+    for cat in font.cataleg_set.all():
+        frases += cat.frases + ";"
+    return render(request, 'filtre/detall_font.html', {'font':font, 'noticies': noticies, 'fonts': Font.objects.all(), 'frases': frases })
+
+
 
 def actualitza_font(request, font_id):
-  f = get_object_or_404(Font, pk=font_id)
-  try:
-    page = requests.get(f.url)
-  except:
-    f.haserror = True
-    return HttpResponseRedirect(reverse('filtre:detall font', args=(f.id,)))
-  if page.status_code != 200:
-    f.haserror = True
-    return HttpResponseRedirect(reverse('filtre:detall font', args=(f.id,)))
-  tree = html.fromstring(page.text)
-  dataset = tree.xpath(f.path)
-  catalegs = f.cataleg_set.all()
-  keys = []
-  for cat in catalegs:
-    keys += re.split('[;:, \n]',cat.frases)
-  #FUTURE WORK:
-  #añadir texto y url
-  #Comprobar errores (no fallar si no hay internet)
-  for dat in dataset:
-    if not f.noticia_set.filter(titol__exact = dat).exists():
-      for key in keys:
-        if dat.lower().find(key) > -1:
-          n = Noticia(titol=dat,data=timezone.now(),font=f)
-          n.save()
-          break
 
-  return HttpResponseRedirect(reverse('filtre:detall font', args=(f.id,)))
+    f = get_object_or_404(Font, pk=font_id)
+    
+    try:
+        page = requests.get(f.url)
+    except:
+        f.haserror = True
+        return HttpResponseRedirect(reverse('filtre:detall font', args=(f.id,)))
+    
+    if page.status_code != 200:
+        f.haserror = True
+        return HttpResponseRedirect(reverse('filtre:detall font', args=(f.id,)))
+    
+    tree = html.fromstring(page.text)
+    dataset = tree.xpath(f.path)
+    catalegs = f.cataleg_set.all()
+    keys = []
+    
+    for cat in catalegs:
+        keys += re.split('[;:, \n]',cat.frases)
+    
+    #FUTURE WORK:
+    #añadir texto y url
+    #Comprobar errores (no fallar si no hay internet)
+    
+    for dat in dataset:
+        if not f.noticia_set.filter(titol__exact = dat).exists():
+            for key in keys:
+                if dat.lower().find(key) > -1:
+                    n = Noticia(titol=dat,data=timezone.now(),font=f)
+                    n.save()
+                    break
+
+    return HttpResponseRedirect(reverse('filtre:detall font', args=(f.id,)))
