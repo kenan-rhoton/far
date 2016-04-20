4b53fb7349ca9104cbc0ed46630d494a7dfed579
Kenan Rhoton <kenan.rhoton@gmail.com>
2016-04-20 16:18:52 +0200

    Implemented Redis Queue

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn far.wsgi --log-file -
+worker: python worker.py
--- filtre/views.py
+++ filtre/views.py
@@ -10,6 +10,11 @@ import re
 
 from .models import Avis, Font
 
+from rq import Queue
+from worker import conn
+
+q = Queue(Connection=conn)
+
 #logger = logging.getLogger(__name__)
 
 def index(request):
@@ -38,6 +43,10 @@ def detall_font(request, font_id):
     return render(request, 'filtre/detall_font.html', {'font':font, 'avisos': avisos, 'fonts': Font.objects.all(), 'frases': frases })
 
 def comprova_font(request, font_id):
+    result = q.enqueue(comprovar_font, font_id)
+    return HttpResponseRedirect(reverse('filtre:detall font', args=(f.id,)))
+
+def comprovar_font(request, font_id):
     f = get_object_or_404(Font, pk=font_id)
 
     import urllib.request
